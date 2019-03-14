fb4aabbfdc7339e48121f94202469e31af0e720a
xuxu99 <48484711+xuxu99@users.noreply.github.com>
2019-03-14 09:55:05 +0100

    modif

--- Batiment.java
+++ Batiment.java
@@ -1,15 +1,30 @@
-import java.util.LinkedList;
+import java.util.LinkedList; 
 
 public class Batiment{
 	double coef;
 	LinkedList <Depenses> liste = new LinkedList();
 	int niveau;
-	int x;
-	int y;
+	int []x;
+	int []y;
+	String image;
 	
 	public Batiment (int x, int y){
-		this.x=x;
-		this.y=y;
+		switch (niveau){
+			case 1:
+			this.x= new int[1];
+			this.y=new int [1];
+			break;
+			case 2:
+			this.x= new int [2];
+			this.y=new int [1];
+			break;
+			case 3:
+			this.x= new int [2];
+			this.y=new int [2];
+			break;
+		}
+		this.x[0]=x;
+		this.y[0]=y;
 	}
 	
 	public void fusion(Batiment b1, Batiment b2){
@@ -17,13 +32,10 @@ public class Batiment{
 	}
 	
 	public void deplacer(int newx, int newy){
-		x=newx;
-		y=newy;
+		x[0]=newx;
+		y[0]=newy;
 	}
 	
-	public double genereArgent(){
-		return 1;
-	}
 	
 	public String toString(){
 		return ("Construction d'un bâtiment de niveau " + niveau + " de coefficient " + coef + " à la position (" + x + "," + y + ")") ;
