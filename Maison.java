836986a0d0bfc77c9feba56245f39def420f1ae5
xuxu99 <48484711+xuxu99@users.noreply.github.com>
2019-04-11 14:41:04 +0200

    attribuerIM dans les filles batiments

--- Maison.java
+++ Maison.java
@@ -4,6 +4,23 @@ public class Maison extends Batiment{
 		super (x,y);
 		coef = 0.1;		
 	}
+	public void attribuerIm(){
+		switch (niveau){
+			case 1:
+			image = "\"media\"maison n1";	
+			break;
+			case 2:
+			image = "\"media\"maison n2";
+			break;
+			case 3:
+			image ="\"media\"maison n3";
+			break;
+		}
+	}
+	
+	public String toString(){
+		return " : Maison ";
+	}
 	
 }
 
